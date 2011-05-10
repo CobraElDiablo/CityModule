812c1512abc3b92b404c059aeb42aba34e06a0d2
CobraElDiablo <cobraeldiablo@yahoo.co.uk>
2011-05-10 02:53:33 +0100

    Added interface to framework for city buildings.

---
+++ Framework/Interfaces/ICityBuilding.cs
@@ -0,0 +1,13 @@
+﻿using OpenMetaverse;
+using OpenSim.Region.Framework.Scenes;
+using Aurora.Framework;
+
+using Nini;
+using Nini.Config;
+
+namespace Aurora.Framework
+{
+    public interface ICityBuilding
+    {
+    }
+}
